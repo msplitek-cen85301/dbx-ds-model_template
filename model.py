51cb61ffc4d70feff32246e6e2480be1873e057b
msplitek@csas.cz <msplitek@csas.cz>
2023-07-17 11:26:02 +0000

    save

--- model.py
+++ model.py
@@ -9,6 +9,56 @@
 
 # COMMAND ----------
 
+!pip install unidecode
+
+# COMMAND ----------
+
+import pandas as pd
+import numpy as np
+
+import re
+from unidecode import unidecode
+
+from sklearn.impute import SimpleImputer
+# from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+
+import mlflow
+import mlflow.sklearn
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Auxiliary funtions
+
+# COMMAND ----------
+
+def ColumnNameNormalization(input_str):
+    # Translate diacritics and remove any non-alphanumeric characters except underscores
+    output_str = re.sub(r'[^a-zA-Z0-9_]', '', unidecode(input_str).lower())
+    
+    # Remove leading digits and add an underscore if the name starts with a digit
+    if output_str and output_str[0].isdigit():
+        output_str = '_' + output_str.lstrip('0123456789')
+    
+    # Ensure the name starts with a letter
+    if output_str and not output_str[0].isalpha():
+        output_str = '_' + output_str
+    
+    return output_str
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Variable definitions
+
+# COMMAND ----------
+
+target = 'target'
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Base load
 # MAGIC    - target definiton?
@@ -25,16 +75,80 @@
 
 data = spark.sql('select * from sbx_ci_catalog.msp_database.cm2_baze_pokusny_vzorek')
 
-data.display()data.display()
+df = data.select("*").toPandas()
+
+df.display()
 
 # COMMAND ----------
 
 # MAGIC %md
 # MAGIC ## Data preparation
 # MAGIC    - remove columns with all missing values
-# MAGIC    - missing imputation
 # MAGIC    - standard scaling
 # MAGIC    - one hot encoding
+# MAGIC    - missing imputation
+
+# COMMAND ----------
+
+df = df.dropna(axis=1, how='all')
+
+Y = df.target
+X = df.loc[:, df.columns != target]
+
+technical_columns = ['pt_unified_key','edt','timestamp']
+# categorical_columns = X.select_dtypes(include='object').columns.tolist()
+categorical_columns = ['vzdelani', 'pohlavi', 'marital_status', 'orgh_region_name', 'ptst_city_size_cat', 'ZIVOTNI_FAZE']
+
+numerical_columns = [x for x in X.columns if x not in categorical_columns and x not in technical_columns]
+
+display(X)
+
+# COMMAND ----------
+
+Imputation = SimpleImputer(missing_values=np.nan, strategy='mean')
+
+X_num = X[numerical_columns]
+X_num_array = Imputation.fit_transform(X_num)
+
+# COMMAND ----------
+
+Scaler = StandardScaler()
+
+X_num = pd.DataFrame(Scaler.fit_transform(X_num_array) , index=X_num.index, columns=X_num.columns)
+
+display(X_num)
+
+# COMMAND ----------
+
+OneHot = OneHotEncoder()
+
+X_cat = X[categorical_columns]
+X_cat = OneHot.fit_transform(X_cat).toarray()
+
+onehot_column_names = OneHot.get_feature_names_out(input_features=categorical_columns)
+onehot_column_names = [ColumnNameNormalization(x) for x in onehot_column_names]
+
+X_cat = pd.DataFrame(X_cat, columns=onehot_column_names)
+
+display(X_cat)
+
+# COMMAND ----------
+
+with mlflow.start_run(run_name='MSP testing'):
+    mlflow.log_param("imputer_strategy", "mean")
+    mlflow.log_param("scaler_strategy", "StandardScaler")
+    mlflow.log_param("encoder_strategy", "OneHotEncoder")
+    
+    # Log the imputer and scaler models to MLflow
+    mlflow.sklearn.log_model(Imputation, "imputer_model")
+    mlflow.sklearn.log_model(Scaler, "scaler_model")
+    mlflow.sklearn.log_model(OneHot, "encoder_model")
+    
+    preprocessed_df = pd.concat([X_num, X_cat], axis=1)
+
+# COMMAND ----------
+
+
 
 # COMMAND ----------
 
